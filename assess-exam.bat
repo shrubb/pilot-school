f6ea5725c7f48a80d7e1ba5a5c0a8f58ab255528
Egor Burkov <e.burkov@samsung.com>
2022-01-10 23:59:55 +0300

    Remove files from 2019

--- assess-exam.bat
+++
@@ -1,3 +0,0 @@
-frc2ascii %1 tmp-flight.txt
-python assess.py Exam/ExamNew.csv tmp-flight.txt --debug
-pause
--- assess-turns-test.bat
+++
@@ -1,3 +0,0 @@
-frc2ascii %1 tmp-flight.txt
-python assess.py TurnsTest/TurnsTest.csv tmp-flight.txt --debug
-pause
